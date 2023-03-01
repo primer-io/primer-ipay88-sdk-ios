d4a9b2a0f333e1dcb28f43feaf839b92c713bf76
Evangelos Pittas <evangelospittas@gmail.com>
2023-03-01 09:41:14 +0200

    Bump `MH` SDK

--- PrimerIPay88SDK/lib/Ipay.h
+++ PrimerIPay88SDK/lib/Ipay.h
@@ -6,22 +6,22 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "IpayPayment.h"
+#import <WebKit/WebKit.h>
 
 @protocol PaymentResultDelegate <NSObject>
 
-- (void)paymentSuccess:(NSString *)refNo withTransId:(NSString *)transId withAmount:(NSString *)amount withRemark:(NSString *)remark withAuthCode:(NSString *)authCode;
+- (void)paymentSuccess:(NSString *)refNo withTransId:(NSString *)transId withAmount:(NSString *)amount withRemark:(NSString *)remark withAuthCode:(NSString *)authCode withTokenId:(NSString *)tokenId withCCName:(NSString *)ccName withCCNo:(NSString *)ccNo withS_bankname:(NSString *)s_bankname withS_country:(NSString *)s_country;
 
+- (void)paymentFailed:(NSString *)refNo withTransId:(NSString *)transId withAmount:(NSString *)amount withRemark:(NSString *)remark  withTokenId:(NSString *)tokenId withCCName:(NSString *)ccName withCCNo:(NSString *)ccNo withS_bankname:(NSString *)s_bankname withS_country:(NSString *)s_country withErrDesc:(NSString *)errDesc;
 
-- (void)paymentFailed:(NSString *)refNo withTransId:(NSString *)transId withAmount:(NSString *)amount withRemark:(NSString *)remark withErrDesc:(NSString *)errDesc;
-
-- (void)paymentCancelled:(NSString *)refNo withTransId:(NSString *)transId withAmount:(NSString *)amount withRemark:(NSString *)remark withErrDesc:(NSString *)errDesc;
+- (void)paymentCancelled:(NSString *)refNo withTransId:(NSString *)transId withAmount:(NSString *)amount withRemark:(NSString *)remark  withTokenId:(NSString *)tokenId withCCName:(NSString *)ccName withCCNo:(NSString *)ccNo withS_bankname:(NSString *)s_bankname withS_country:(NSString *)s_country withErrDesc:(NSString *)errDesc;
 
 - (void)requerySuccess:(NSString *)refNo withMerchantCode:(NSString *)merchantCode withAmount:(NSString *)amount withResult:(NSString *)result;
 
 - (void)requeryFailed:(NSString *)refNo withMerchantCode:(NSString *)merchantCode withAmount:(NSString *)amount withErrDesc:(NSString *)errDesc;
 @end
 
-@interface Ipay : UIViewController <UIWebViewDelegate, NSURLConnectionDelegate, UIScrollViewDelegate> {
+@interface Ipay : UIViewController <NSURLConnectionDelegate, UIScrollViewDelegate,WKNavigationDelegate,WKUIDelegate> {
     __weak id <PaymentResultDelegate> delegate;
 }
 @property (nonatomic,weak) id <PaymentResultDelegate> delegate;
@@ -29,5 +29,9 @@
 - (UIView *)checkout:(IpayPayment *)payment;
 
 - (void)requery:(IpayPayment *)payment;
+
+- (void)handleLinkFromCustomSchemeURL:(NSURL*)url;
+
++ (id)sharedManager;
 
 @end
