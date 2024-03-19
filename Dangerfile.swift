9718f9ba0aebe425d43cd217b83081cef364a130
Niall Quinn <niall@primer.io>
2024-03-19 16:36:21 +0100

    update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -13,7 +13,11 @@ func isConventionalCommitTitle() -> Bool {
     pr.title.hasPrefix("feat:")
 }
 
-if !isReleasePr && !isConventionalCommitTitle() {
+// MARK: - Conventional Commit Title
+let validPrefixes = ["fix", "feat", "chore", "ci", "refactor", "docs",
+                     "perf", "test", "build", "revert", "style", "BREAKING CHANGE"]
+let isConventionalCommitTitle = validPrefixes.contains { pr.title.hasPrefix($0) }
+
+if !pr.head.ref.hasPrefix("release") && !isConventionalCommitTitle {
     fail("Please use a conventional commit title for this PR. See [Conventional Commits and SemVer](https://www.notion.so/primerio/Automating-Version-Bumping-and-Changelog-Creation-c13e32fea11447069dea76f966f4b0fb?pvs=4#c55764aa2f2748eb988d581a456e61e7)")
 }
-
