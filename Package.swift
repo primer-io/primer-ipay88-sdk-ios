1d387eb94d24ce250c91e0937a7edac8f1edac2f
Jack Newcombe <jack.newcombe@primer.io>
2024-05-31 11:13:55 +0100

    Try using static product

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "PrimerIPay88MYSDK",
+            type: .static,
             targets: [
                 "PrimerIPay88MYSDK",
                 "libipay88sdk"
